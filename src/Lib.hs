3a7e71f8956ef4ea7040a19ebad3e8db55a97e97
Gabi314 <gcanteroschuquipoma@frba.utn.edu.ar>
2021-06-15 11:22:02 -0300

    comentario 3

--- src/Lib.hs
+++ src/Lib.hs
@@ -12,12 +12,12 @@ doble= (*2)
 
 --Sabemos que el atraco lo realizan ladrones de profesión, de los cuales se conocen su nombre, habilidades
 -- y las armas que lleva.
+
 data Ladron {
     nombre=String
     habilidades=[Habilidad]
     armas= [Armas]
 }
-
 
 --De los rehenes conocemos su nombre, su nivel de complot, su nivel de miedo, y su plan contra los ladrones, 
 --la cual puede involucrar a algún otro rehén.
