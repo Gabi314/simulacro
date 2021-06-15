c4e3e1d0d37ec01ac8b37f0c5fec10438a4f8063
Gabi314 <gcanteroschuquipoma@frba.utn.edu.ar>
2021-06-15 11:14:20 -0300

    comentario 2

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,5 +8,16 @@ someFunc = putStrLn "someFunc"
 doble :: Int->Int
 doble= (*2)
 
+--De antemano, nos piden que modelemos a los ladrones y los rehenes.
+
+--Sabemos que el atraco lo realizan ladrones de profesión, de los cuales se conocen su nombre, habilidades
+-- y las armas que lleva.
+data Ladron {
+    nombre=String
+    habilidades=[Habilidad]
+    armas= [Armas]
+}
 
 
+--De los rehenes conocemos su nombre, su nivel de complot, su nivel de miedo, y su plan contra los ladrones, 
+--la cual puede involucrar a algún otro rehén.
